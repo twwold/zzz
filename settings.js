645ac10a38bb53b3ea6f7e461150a7e3b462abc5
twwold <122291133+twwold@users.noreply.github.com>
2024-03-26 14:15:57 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,83 @@
+/*
+
+
+███████╗██╗░░░██╗░██████╗██╗░░░██╗░█████╗░░█████╗░  ░░░░░░  ███╗░░░███╗██████╗░
+╚════██║██║░░░██║██╔════╝╚██╗░██╔╝██╔══██╗██╔══██╗  ░░░░░░  ████╗░████║██╔══██╗
+░░███╔═╝██║░░░██║╚█████╗░░╚████╔╝░██║░░╚═╝██║░░██║  █████╗  ██╔████╔██║██║░░██║
+██╔══╝░░██║░░░██║░╚═══██╗░░╚██╔╝░░██║░░██╗██║░░██║  ╚════╝  ██║╚██╔╝██║██║░░██║
+███████╗╚██████╔╝██████╔╝░░░██║░░░╚█████╔╝╚█████╔╝  ░░░░░░  ██║░╚═╝░██║██████╔╝
+╚══════╝░╚═════╝░╚═════╝░░░░╚═╝░░░░╚════╝░░╚════╝░  ░░░░░░  ╚═╝░░░░░╚═╝╚═════╝░
+
+
+
+Project Name : ZUSYCO - MD
+Creator : @darkmakerofc , @mr-nima-x 
+
+*/
+
+module.exports = {
+  OWNER_NUMBER: "#999",
+  SUDO_NUMBERS : "94678765432,946532567",
+  OWNER_NAME: "『 ꧁⃟тωσяℓ∂⃢м∂вσт〠⃤꧂ 』",
+  BOT_NAME : "TworldBOT",
+  ZUSYCO_FILE : "ZUSYCO-QR",
+  LOGOS: {
+    ALIVE: 'https://telegra.ph/file/0851f6466151750eb5908.jpg',
+    WELCOME: 'https://telegra.ph/file/0851f6466151750eb5908.jpg',
+    GOODBYE: 'https://telegra.ph/file/0851f6466151750eb5908.jpg'
+  },
+  STOP_SEND : {
+        WELCOME_MESSAGE : false,
+        GOODBYE_MESSAGE : true
+    },
+  DESABLE_WELCOME_GOODBYE_JIDS : [],
+  TIME_ZONE :  "Asia/Colombo",
+  WORK_TYPE : "public",
+  WORK_MODE : "onlygroups",
+  WORK_MODE_MESSAGE_SEND : false,
+  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ꧁⃟тωσяℓ∂⃢м∂вσт〠⃤꧂ ᴍᴅ ʙᴏᴛ',
+  STICKER_PACK : '[ ꧁⃟тωσяℓ∂⃢м∂вσт〠⃤꧂ ]',
+  BAD_WORDS : 'fuck,pussy',
+  ANTI_BAD : false,
+  ANTI_LINKS : 'chat.whatsapp.com,xvideos.com,pornhub.com',
+  ANTI_LINK : true,
+  ANTI_LINK_KICK : false,
+  MAX_SIZE : 100,
+  MOROCCO_NUMBERS : {
+        BLOCK : true,
+        NOT_WORKING : true,
+  },
+  READ : {
+     ALL_MESSAGES : true,
+     ONLY_COMMANDS : false,
+     STATUS : true,
+  },
+  BOT_OFFLINE : false,
+  INBOX_BLOCK : false,
+  AUTO_REACT : true,
+  OWNER_REACT : '🤵‍♂️',
+  AUTO_BGM : false,
+  BGM : {
+    "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
+    "leo" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-leo.mp3",
+    "tance" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-tance0.mp3",
+    "romant" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-floot.mp3",
+    "serio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-serio.mp3",
+    "chill" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-wichil.mp3",
+    "yaka" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-yaka.mp3",
+    "animel" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-animel.mp3",
+    "bitch" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-bitch.mp3",
+    "papa" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-papa.mp3",
+    "shee" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-sheela.mp3",
+    "thugs" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-thugs.mp3",
+
+  },
+  AUTO_STICKER : false,
+  STICKERS : {
+    "pit" : "https://i.ibb.co/NVLMQDP/pit.webp",
+    "agei" : "https://i.ibb.co/yqk8QQ9/agei.webp",
+    "ne" : "https://i.ibb.co/N6Wb6mZ/ne.webp",
+  },
+  LOAD_ANIMATION : false,
+ 
+};
